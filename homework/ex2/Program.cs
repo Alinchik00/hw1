8df0d4e3081b7e6f61bf7764a9f463a29423f06b
Alina <kan.alina2017@yandex.ru>
2022-10-19 23:36:19 +0300

    Решила задачу 2

---
+++ homework/ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberA>numberB)
+{
+    Console.WriteLine(numberA + " наибольшее число");
+}
+else
+{
+    Console.WriteLine(numberB + " наибольшее число");
+}
