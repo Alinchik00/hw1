bda97b1162cb680e111b9387fa2305f6a2e2eccd
Alina <kan.alina2017@yandex.ru>
2022-10-19 23:55:51 +0300

    Решила задачу 4

---
+++ homework/ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число число: ");
+int number3 = int.Parse(Console.ReadLine()!);
+if (number1>number2)
+{
+    if (number1>3)
+    {
+        Console.WriteLine("Наибольшее число " + number1);
+    }
+    else
+    {
+        Console.WriteLine("Наибольшее число " + number3);
+    }
+}
+else
+{
+Console.WriteLine("Наибольшее число " + number2);
+}
