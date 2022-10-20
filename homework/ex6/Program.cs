bcb02ca6ff0928b0febb9ddf0c1c0d2fbed5d0f5
Alina <kan.alina2017@yandex.ru>
2022-10-20 20:36:39 +0300

    задача 6

---
+++ homework/ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int numberB = numberA%2;
+if (numberB<1)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
