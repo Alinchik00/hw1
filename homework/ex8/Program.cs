57dfd97df7356092129a25a1235dd63c06ded7c3
Alina <kan.alina2017@yandex.ru>
2022-10-20 20:59:47 +0300

    Задача 8

---
+++ homework/ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+if (numberA>2)
+{
+    int numberB = 2;
+    Console.WriteLine(numberB);
+numberB = numberB + 2;
+do
+{
+    Console.WriteLine(numberB);
+    numberB = numberB + 2;
+}
+while (numberB<=numberA);
+}
+else
+{
+    Console.WriteLine("нет чисел, удовлетворяющих запросу");
+}
